4dcea0c144463950bba32d22581dce63e756bf70
AcelisWeaven <jeremy@8oclock.fr>
2016-09-13 13:49:33 +0200

    fix upload

--- Resources/public/js/modal-image.js
+++ Resources/public/js/modal-image.js
@@ -106,7 +106,7 @@ if(!window.ModalImage){window.ModalImage = (function(ModalImage) {
 
         var currentWidth = modalCropperImage.cropper('getData').width;
         var currentHeight = modalCropperImage.cropper('getData').height;
-        var maxSize = 8000;
+        var maxSize = 4000;
         var isPortrait = (currentHeight > currentWidth);
 
         var options = {
